d748c27a6f2d13dd02f61ab2aee8a9ddeb0789f4
Ank1r0 <jenerieshope@gmail.com>
2024-02-01 20:44:50 +0100

    BubbleSort done

--- src/algorithms/InsertionSort.java
+++ src/algorithms/InsertionSort.java
@@ -3,8 +3,10 @@ package algorithms;
 public class InsertionSort {
     public static int[] sort(int[] array)
     {
-
-
+        for(int i = 0; i < array.length; ++i)
+        {
+            System.out.println(1);
+        }
         return null;
     }
 }
